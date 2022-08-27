6475fe624f67bcb21935e36502b721adcd6c0368
xvyvx <xyx1985@gmail.com>
2022-08-27 13:19:54 +0800

    build script modify.

--- CMake/BuildUtils/UnitTestHelper.cmake
+++ CMake/BuildUtils/UnitTestHelper.cmake
@@ -7,9 +7,14 @@ function(AddTestCaseToTarget targetName)
     set(Root_TESTCASE_Keywords "FILTER" "COND")
     ParseStructuredLevelParameters("Root" "Root" "${ARGN}")
     foreach(setValueIndex RANGE 0 ${Root_TESTCASE_index})
-        if((NOT DEFINED Root_TESTCASE_${setValueIndex}_COND_0
-                AND DEFINED Root_TESTCASE_${setValueIndex}_Default_0)
-            OR "${Root_TESTCASE_${setValueIndex}_COND_0}")
+        if(DEFINED Root_TESTCASE_${setValueIndex}_COND_0)
+            set(tempUserCond ${Root_TESTCASE_${setValueIndex}_COND_0})
+        elseif(DEFINED Root_TESTCASE_${setValueIndex}_Default_0)
+            set(tempUserCond TRUE)
+        else()
+            set(tempUserCond FALSE)
+        endif()
+        if(${tempUserCond})
             add_test(NAME ${Root_TESTCASE_${setValueIndex}_Default_0}
                 COMMAND ${targetName} --run_test=${Root_TESTCASE_${setValueIndex}_FILTER_0} --log_level=message)
             set_tests_properties(${Root_TESTCASE_${setValueIndex}_Default_0} PROPERTIES PASS_REGULAR_EXPRESSION

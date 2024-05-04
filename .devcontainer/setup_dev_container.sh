057dfd61d6d039734dd36e3588ee463d4d7f7911
xvyvx <mail@xvyvx.me>
2024-05-04 14:10:15 +0900

    Devcontainer improve.

--- .devcontainer/setup_dev_container.sh
+++ .devcontainer/setup_dev_container.sh
@@ -3,5 +3,8 @@
 DevPath=$1
 
 mkdir -p $DevPath/.vscode && cp $DevPath/.devcontainer/cmake-variants.yaml.in $DevPath/.vscode/cmake-variants.yaml
+cp $DevPath/.devcontainer/cmake-kits.json.in $DevPath/.vscode/cmake-kits.json
 rm -f $DevPath/.clangd
 sed "s?@devPath?$DevPath?g" $DevPath/.devcontainer/.clangd.in > $DevPath/.clangd
+mkdir -p $DevPath/build/gcc-x86_64-linux-gnu/Debug && \
+ln -s $DevPath/build/gcc-x86_64-linux-gnu/Debug/compile_commands.json $DevPath/.vscode/compile_commands.json
